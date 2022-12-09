123d1b2f1752a22e23abdf9d9433f2626cea5ec2
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-12-09 16:58:42 +0200

    Create api-uploadImages.js

---
+++ shared/api/api-uploadImages.js
@@ -0,0 +1,35 @@
+import { nanoid } from "nanoid";
+import db from "../../firebase/config";
+
+export async function takePhoto(camera, setUri, setLocationCoords) {
+  const { uri } = await camera.takePictureAsync();
+  setUri(uri);
+  const data = await Location.getCurrentPositionAsync();
+  setLocationCoords({
+    latitude: data.coords.latitude,
+    longitude: data.coords.longitude,
+  });
+}
+export async function uploadPhotoToServer(uri, nameCollection) {
+  try {
+    const response = await fetch(uri);
+    const file = await response.blob();
+    const uniquePostId = nanoid().toString();
+    await db.storage().ref(`${nameCollection}`).child(uniquePostId).put(file);
+
+    const urlPhotoStorage = await db
+      .storage()
+      .ref(`${nameCollection}`)
+      .child(uniquePostId)
+      .getDownloadURL();
+
+    return urlPhotoStorage;
+  } catch (error) {
+    console.log(
+      `%c[Error - uploadPhotoToServer(): ${error.message}]`,
+      "color: #F44336;"
+    );
+
+    throw error;
+  }
+}
