9825ee9b9e2644719bc8d9c3cdd7a5a3280b26b4
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-12-13 01:27:40 +0200

    Update PostsScreen.js

--- screens/MainScreen/PostsScreen.js
+++ screens/MainScreen/PostsScreen.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { moduleName } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "../NestedScreens/Home";
@@ -11,11 +10,7 @@ const NestedScreen = createStackNavigator();
 export default function PostsScreen() {
   return (
     <NestedScreen.Navigator>
-      <NestedScreen.Screen
-        name="Home"
-        component={Home}
-        options={{ headerShown: false }}
-      />
+      <NestedScreen.Screen name="Публикации" component={Home} />
       <NestedScreen.Screen name="Комментарии" component={CommentsScreen} />
       <NestedScreen.Screen name="Карта" component={MapScreen} />
     </NestedScreen.Navigator>
