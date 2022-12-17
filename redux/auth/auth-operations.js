564a2f49dd3182d577668108871e6fd0ebce9da6
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-12-17 23:43:16 +0200

    Update auth-operations.js

--- redux/auth/auth-operations.js
+++ redux/auth/auth-operations.js
@@ -1,6 +1,12 @@
 import { createOperation } from "../../shared/utils/utils";
 
-import { signUp, signIn, signOut, checkAuth } from "../../shared/api/api-auth";
+import {
+  signUp,
+  signIn,
+  signOut,
+  checkAuth,
+  updateProfilePhoto,
+} from "../../shared/api/api-auth";
 
 export const authSignUp = createOperation("auth/signup", signUp);
 export const authSignIn = createOperation("auth/signin", signIn);
@@ -8,3 +14,8 @@ export const authSignIn = createOperation("auth/signin", signIn);
 export const authSignOut = createOperation("auth/signout", signOut);
 
 export const authCheckAuth = createOperation("auth/checkAuth", checkAuth);
+
+export const authUpdateProfilePhoto = createOperation(
+  "auth/updateProfilePhoto",
+  updateProfilePhoto
+);
